de920c1ad648f0450dc9350310f5d8499992cdc2
Kamran Mansoor <kamranmansoor36@gmail.com>
2023-11-06 23:18:33 +0500

    All Files for Mid AdvancedWEB

--- Controller/index.js
+++ Controller/index.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const userRoutes = require("./Routes/users"); 
-const taskRoutes = require("./Routes/tasks");
+// asic Node.js application using the Express.js framework 
+// to create a simple RESTful API for managing user and task information.
 
-const app = express();
-app.use("/user", userRoutes); 
-app.use("/task", taskRoutes);
+const express = require("express"); // Import express
+const userRoutes = require("./Routes/users"); // Import userRoutes
+const taskRoutes = require("./Routes/tasks"); // Import taskRoutes
 
-const PORT = process.env.PORT || 3000; 
+const app = express(); // Make express app
+app.use("/user", userRoutes); // If accessing localhost:3000/user, it will go to userRoutes
+app.use("/task", taskRoutes); // If accessing localhost:3000/task, it will go to taskRoutes
 
+const PORT = process.env.PORT || 3000; // Make PORT variable
+
+// Make server listen to PORT
 app.listen(PORT, () => { 
     console.log(`Listening to: ${PORT}`);
 });
